e3971219881f73070bcc590e0ed5c79999cea6bb
b00nw33 <git@stackblitz.com>
2025-01-17 13:06:11 +0800

    Initial commit

---
+++ src/OurComp.jsx
@@ -0,0 +1,13 @@
+function OurComp() {
+  return (
+    <div>
+      My work according to YouTube tutorial
+      <br />
+      <a href="https://www.youtube.com/watch?v=OA5JAmTcTz4">
+        React JS Tutorial For Beginners 2025
+      </a>
+    </div>
+  );
+}
+
+export default OurComp;
